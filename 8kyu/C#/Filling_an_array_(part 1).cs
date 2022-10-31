00241a969094f0d9b6ed88167b4c02aa4a1c35c9
Ashley Littlewood <ashley.littlewood22@gmail.com>
2022-10-31 22:49:40 +0000

    filling an array

---
+++ 8kyu/C#/Filling_an_array_(part 1).cs
@@ -0,0 +1,21 @@
+// We want an array, but not just any old array, an array with contents!
+
+// Write a function that produces an array with the numbers 0 to N-1 in it.
+
+// For example, the following code will result in an array containing the numbers 0 to 4:
+
+// arr(5) // => [0,1,2,3,4]
+// Note: The parameter is optional. So you have to give it a default value.
+
+public class Kata
+{
+  public static int[] Arr(int N)
+  {
+    int[] newArray = new int[N];
+    for(int i=0; i < N; i++)
+    {
+      newArray[i]=i;
+    }
+    return newArray;
+  }
+}
