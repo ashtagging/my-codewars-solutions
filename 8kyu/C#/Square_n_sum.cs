4c3775735ecb88809416cf975e4381fa02dc50fe
Ashley Littlewood <ashley.littlewood22@gmail.com>
2022-11-03 23:14:27 +0000

    Square(n)Sum

---
+++ 8kyu/C#/Square_n_sum.cs
@@ -0,0 +1,17 @@
+// Complete the square sum function so that it squares each number passed into it and then sums the results together.
+
+// For example, for [1, 2, 2] it should return 9 because 1^2 + 2^2 + 2^2 = 9.
+
+using System;
+
+public static class Kata
+{
+  public static int SquareSum(int[] numbers)
+  { 
+    int sum = 0;
+    foreach(int integer in numbers){
+      sum += (integer*integer);
+    }
+      return sum;
+  }
+}
