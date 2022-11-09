64b3241a2fae7ed364aee5d4e3e09519b0531487
Ashley Littlewood <ashley.littlewood22@gmail.com>
2022-11-09 17:51:28 +0000

    Beginner series paperwork

--- 6kyu/C#/Sum_of_Digits_Digital_Root.cs
+++ 6kyu/C#/Sum_of_Digits_Digital_Root.cs
@@ -12,14 +12,12 @@ using System;
 using System.Linq;
 
 public class Number
-{
-     public int DigitalRoot(long n)
+{ public int DigitalRoot(long n)
+    {
+        while (n > 9)
             {
-                // Your awesome code here!
-               while (n > 9)
-                {
-                    n = n.ToString().Select(c => Convert.ToInt32(c.ToString())).Sum();
-                }
-                return (int)n;
+                n = n.ToString().Select(c => Convert.ToInt32(c.ToString())).Sum();
             }
+            return (int)n;
+    }
 }
