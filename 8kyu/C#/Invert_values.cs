c8cc7205a51210a185ace03a7e0badf34a9140fc
Ashley Littlewood <ashley.littlewood22@gmail.com>
2022-11-30 21:38:09 +0000

    Invert Values

---
+++ 8kyu/C#/Invert_values.cs
@@ -0,0 +1,18 @@
+// Given a set of numbers, return the additive inverse of each. Each positive becomes negatives, and the negatives become positives.
+
+// invert([1,2,3,4,5]) == [-1,-2,-3,-4,-5]
+// invert([1,-2,3,-4,5]) == [-1,2,-3,4,-5]
+// invert([]) == []
+
+using System.Linq;
+
+namespace Solution
+{
+  public static class ArraysInversion
+  {
+    public static int[] InvertValues(int[] input)
+    {
+      return input.Select(i => i*-1).ToArray();
+    }
+  }
+}
