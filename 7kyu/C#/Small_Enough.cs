1f0b49b1930162a725ac8ac1cbe0c6ca8255d250
Ashley Littlewood <ashley.littlewood22@gmail.com>
2022-12-06 07:50:04 +0000

    Small enough

---
+++ 7kyu/C#/Small_Enough.cs
@@ -0,0 +1,18 @@
+// You will be given an array and a limit value. You must check that all values in the array are below or equal to the limit value. If they are, return true. Else, return false.
+
+// You can assume all values in the array are numbers.
+
+using System;
+
+public class Kata
+{
+  public static bool SmallEnough(int[] a, int limit)
+  {
+    for(int i=0; i < a.Length;i++){
+      if(a[i] > limit){      
+        return false;
+      } 
+    }
+    return true;
+  }
+}
