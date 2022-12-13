445d79375f297e5eaf1c3a0884896c6b7d7aa167
Ashley Littlewood <ashley.littlewood22@gmail.com>
2022-12-13 22:22:48 +0000

    Calculate Average

---
+++ 8kyu/C#/Calculate_Average.cs
@@ -0,0 +1,14 @@
+// Write a function which calculates the average of the numbers in a given list.
+
+// Note: Empty arrays should return 0.
+
+using System;
+using System.Linq;
+
+class AverageSolution
+{
+  public static double FindAverage(double[] array)
+  {
+    return array.Length == 0 ? 0 : ((array.Sum())/(array.Length));
+  }
+} 
