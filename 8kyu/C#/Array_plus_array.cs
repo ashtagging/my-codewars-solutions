2992c03b3f57175709cc9931ea4a898c0d314deb
Ashley Littlewood <ashley.littlewood22@gmail.com>
2023-01-25 17:14:55 +0000

    Array plus array

---
+++ 8kyu/C#/Array_plus_array.cs
@@ -0,0 +1,13 @@
+// I'm new to coding and now I want to get the sum of two arrays... Actually the sum of all their elements. I'll appreciate for your help.
+
+// P.S. Each array includes only integer numbers. Output is a number too.
+
+using System.Linq;
+
+public static class Kata
+{
+  public static int ArrayPlusArray(int[] arr1, int[] arr2)
+  {
+    return (arr1.Sum()) + (arr2.Sum());
+  }
+}
