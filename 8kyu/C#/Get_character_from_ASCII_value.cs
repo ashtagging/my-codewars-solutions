bc76279d568388976914d468cd31c14aeefd043b
Ashley Littlewood <ashley.littlewood22@gmail.com>
2023-04-13 22:29:34 +0100

    get character from ASCII value

---
+++ 8kyu/C#/Get_character_from_ASCII_value.cs
@@ -0,0 +1,17 @@
+// Write a function get_char() / getChar() which takes a number and returns the corresponding ASCII char for that value.
+
+// Example:
+
+// get_char(65)
+// should return:
+
+// 'A'
+// For ASCII table, you can refer to http://www.asciitable.com/
+
+public class Kata
+{
+  public static char GetChar(int charcode)
+  {
+    return (char)charcode;
+  }
+}
