c8b2974cce2225a4e5fdb42b37da5caa3f864962
Ashley Littlewood <ashley.littlewood22@gmail.com>
2023-06-18 17:44:39 +0100

    Find numbers which are divisible by given number

---
+++ 8kyu/C#/Find numbers which are divisible by given number.cs
@@ -0,0 +1,14 @@
+// Complete the function which takes two arguments and returns all numbers which are divisible by the given divisor. First argument is an array of numbers and the second is the divisor.
+
+// Example(Input1, Input2 --> Output)
+// [1, 2, 3, 4, 5, 6], 2 --> [2, 4, 6]
+
+using System.Linq;
+
+public class Kata
+{
+  public static int[] DivisibleBy(int[] numbers, int divisor)
+  {
+    return numbers.Where(n => n % divisor == 0).ToArray();
+  }
+}
